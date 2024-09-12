877752234650eefdb0c89a81dca9cc7360d17929
Matt <84277551+MattRueter@users.noreply.github.com>
2024-09-11 22:40:38 -0500

    remove tutorial files.

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/overview">
-            See overview
+            Go to the docs
           </Link>
         </div>
       </div>
