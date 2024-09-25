a428979b6e8311a68354c92a5f43cd37dd95da37
Matt <84277551+MattRueter@users.noreply.github.com>
2024-09-25 11:56:15 -0500

    exclude work in progress from rendering in sidebar.

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -41,10 +41,9 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
+          exclude: ['**/client/**', '**/server/**'],
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
@@ -54,8 +53,6 @@ const config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
