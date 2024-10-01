060bda11ca325b2d24f8a41a1f1e706893a64100
Matt <84277551+MattRueter@users.noreply.github.com>
2024-10-01 17:44:46 -0500

    config gh pages.

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'img/icon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://parlanchin.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/idiom-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Parlanchin', // Usually your GitHub org/user name.
+  projectName: 'idiom-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
